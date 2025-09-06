facc23568da307e0dc90d97d3770391109bc6c55
Narendra Nareda <narendradmirer145@gmail.com>
2025-09-06 22:33:26 +0530

    custom validations

--- user_api.py
+++ user_api.py
@@ -1,45 +1,31 @@
 from fastapi import APIRouter, Query, Path
 from user_model import UserCreate, UserResponse
-from typing import List
+from typing import List, Union, Dict
 
 router = APIRouter()
-id_counter = 3
-user_list = [
-    {
-        "name": "Narendra",
-        "age": 28,
-        "id": 1,
-        "password": "12892037hafdsjl"
-    },
-    {
-        "name": "Govind",
-        "age": 25,
-        "id": 2,
-        "password": "1213"
-    }
-]
+id_counter = 1
+global_user_dict = {}
 
 
 @router.get("/")
 def get_all_user():
-    return {"users": user_list}
+    return {"users": global_user_dict}
 
 
-@router.get("/{id}")
+@router.get("/{id}", response_model=Union[UserResponse, Dict])
 def get_user_by_id(id: int = Path(..., gt=0), 
                    age: int = Query(..., ge=0, le=150)):
-    for user in user_list:
-        if user["id"] == id:
-            return user
+    user_resp = global_user_dict.get(id)
+    if user_resp is not None:
+        return user_resp
     return {}
 
 
-@router.post("/", response_model=List[UserResponse])
+@router.post("/", response_model=Dict[int, UserResponse])
 def create_user(user: UserCreate):
     global id_counter
     user_dict = user.dict()
     user_dict.update(id=id_counter)
+    global_user_dict[id_counter] = user_dict
     id_counter += 1
-    user_list.append(user_dict)
-
-    return user_list
+    return global_user_dict
--- user_model.py
+++ user_model.py
@@ -1,15 +1,46 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator, model_validator
+from datetime import datetime
+import pytz
 
 
-class UserCreate(BaseModel):
-    name: str
-    age: int
-    password: str = Field(..., min_length=6, max_length=12)
+class Address(BaseModel):
+    city: str
+    state: str
+    pincode: str
 
-
-
-class UserResponse(BaseModel):
-    id: int
-    name: str
-    age: int
+    @field_validator("pincode")
+    def pincode_validate(cls, v):
+        if (len(v) != 6 ) or (not v.isdigit()):
+            raise ValueError("pincode must be six letter")
+        return v
     
+    @model_validator(mode="before")
+    def multi_validate(cls, d):
+        states = d.get("state")
+        state_list = ["mp", "up", "string"]
+        if states not in state_list:
+            raise ValueError("Invalid state provided")
+        return d
+
+
+class UserBase(BaseModel):
+    name: str
+    age: int
+    address: Address
+
+
+
+class UserCreate(UserBase):
+    password: str = Field(..., min_length=6, max_length=12)
+   
+
+
+def calculate_time():
+    time_now = datetime.now()
+    time_now.astimezone(tz=pytz.timezone("Asia/Kolkata"))
+    return time_now
+
+
+class UserResponse(UserBase):
+    id: int
+    date_created: datetime = Field(default_factory=calculate_time)
